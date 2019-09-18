fd838caa3347c4942fb7253551a0ebbc3d4ccd24
Recai Adar <recai.adar@gfk.com>
2019-09-18 14:15:25 +0200

    Fix for image path

--- Movies/Controllers/ImageController.cs
+++ Movies/Controllers/ImageController.cs
@@ -13,20 +13,24 @@ namespace Movies.Controllers
     public class ImageController : Controller
     {
         private readonly IImageService _imageService;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public ImageController(IImageService imageService)
+        public ImageController(IImageService imageService, IHostingEnvironment hostingEnvironment)
         {
             _imageService = imageService;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public IActionResult Index()
         {
             System.Drawing.Image image = _imageService.DownloadImageFromUrl("https://picsum.photos", 200, 300);
+
+            var imageDirectory = _hostingEnvironment.WebRootPath + "\\images";
+            if (!Directory.Exists(imageDirectory))
+                Directory.CreateDirectory(imageDirectory);
+
+            var fileName = Path.Combine(imageDirectory, "image.jpg");
             
-            var imagePath = AppDomain.CurrentDomain.BaseDirectory + "\\images";
-            if (!Directory.Exists(imagePath))
-                Directory.CreateDirectory(imagePath);
-            string fileName = Path.Combine(imagePath, "image.jpg");
             image.Save(fileName);
             return View();
         }
